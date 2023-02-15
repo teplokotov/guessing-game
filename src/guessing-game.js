802f535315ba4e33f449595dc5f5414accc8c05e
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-02-16 02:39:53 +0300

    feat: add a solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
     constructor() {}
-
+      
     setRange(min, max) {
-
+        this.low = min;
+        this.high = max;
     }
-
+      
     guess() {
-
+        let range = this.high - this.low;
+        let middle = range / 2 + this.low;
+        this.middleRounded = Math.round(middle);
+        return this.middleRounded;
     }
-
+      
     lower() {
-
+        this.setRange(this.low, this.middleRounded);
     }
-
+      
     greater() {
-
+        this.setRange(this.middleRounded, this.high);
     }
 }
 
